82da05fa1474c5349c4b93befb286e661df4846e
Kowalski Gravity <58356670+Gravity-Zero-o@users.noreply.github.com>
2023-03-27 22:26:27 +0800

    update a sort function

--- plot.py
+++ plot.py
@@ -9,7 +9,7 @@ def extract_data(file_path, output_folder):
             arr.append(line.split())
         global data
         for i in range(len(arr)):
-            if float(arr[i][0]) == 84.32 and float(arr[i][1]) == 54.34:   # 80.57      54.34
+            if float(arr[i][0]) == 73.15 and float(arr[i][1]) == 51.20:   # location 73.15 51.20
                 data.append([float(arr[i][2]), float(arr[i][3])])
 
         '''#this is write the corresponding data to a new file
@@ -23,11 +23,18 @@ def extract_data(file_path, output_folder):
 input_folder = 'K:/tmp/test1/0323/PIVdata'
 output_folder = 'K:/tmp/test1/0323/out'
 plot_folder='K:/tmp/test1/0323/plot'
+files = os.listdir(output_folder)
+files = sorted(files, key=lambda x: int(x[-8:-4]))   # 按照照片编号后四位排序
 for filename in os.listdir(output_folder):
     if filename.endswith('.dat'):
+    #    print(filename)
+        #data.append(filename)                       #把文件名追加到data中，来看输出的文件顺序
         file_path = os.path.join(output_folder, filename)
         extract_data(file_path, plot_folder)
-    output_path = os.path.join(plot_folder, 'plot.dat')
+    output_path = os.path.join(plot_folder, 'plot2.dat')
 with open(output_path, 'w') as f2:
     for line in data:
-        f2.write(" ".join(map(str,line)) + "\n")
+        f2.write("  ".join(map(str,line)) + "\n")
+
+
+
